a2cc2d4be2f8663eee9141cb0339ce1c64f3a47d
rlarjsl03 <405zign@gmail.com>
2025-06-12 00:18:25 +0900

    기말 연습

--- src/Week_14_Prac/MonthSchedule_1.java
+++ src/Week_14_Prac/MonthSchedule_1.java
@@ -3,10 +3,6 @@ package Week_14_Prac;
 import java.util.Scanner;
 
 class Day_1 {
-
-}
-public class MonthSchedule_1 {
-    Scanner sc = new Scanner(System.in);
     private String work;    // 하루의 할 일을 나타내는 문자열
 
     public void set(String work) { this.work = work; }
@@ -15,20 +11,37 @@ public class MonthSchedule_1 {
         if (work == null) System.out.println("없습니다.");
         else System.out.println(work + "입니다.");
     }
-    public void input() {
+}
+public class MonthSchedule_1 {
+    Scanner sc = new Scanner(System.in);
+    Day_1[] date;
 
+    public MonthSchedule_1(int day) {
+        date = new Day_1[day];
+    }
+
+    public void input() {
+        System.out.print("날짜(1~30)? ");
+        int daysel = sc.nextInt();
+        daysel--;
+        date[daysel] = new Day_1();
+        System.out.print("할일? ");
+        String work = sc.next();
+        date[daysel].set(work);
     }
     public void view() {
-
+        System.out.print("날짜(1~30)? ");
+        int daysel2 = sc.nextInt();
+        daysel2--;
+        date[daysel2].show();
     }
     public void finish() {
-
+        System.out.println("프로그램을 종료합니다.");
     }
-
     public void run() { //스케줄 관리 프로그램 실행
         System.out.println("이번달 스케줄 관리 프로그램.");
         while(true) {
-            System.out.println("할일(입력:1, 보기:2, 끝내기:3) >>");
+            System.out.print("할일(입력:1, 보기:2, 끝내기:3) >> ");
             int menu = sc.nextInt();
             switch(menu) {
                 case 1: input(); break;
@@ -39,8 +52,9 @@ public class MonthSchedule_1 {
             }
         }
     }
+
     public static void main(String[] args) {
-//        MonthSchedule_1 april = new MonthSchedule_1(30);
-//        april.run();
+        MonthSchedule_1 april = new MonthSchedule_1(30);
+        april.run();
     }
 }
